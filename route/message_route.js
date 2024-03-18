ca317c1df7e8cef9b94c96b26d01cd151cd980e3
DuongWuangDat <duongquangdat172004@gmail.com>
2024-03-18 09:27:24 +0700

    do smth

--- route/message_route.js
+++ route/message_route.js
@@ -3,5 +3,5 @@ const messageController = require('../controller/message_controller.js')
 const route = express.Router()
 route.post('/send', messageController.sendMessage)
 route.get('/find/:chatID',messageController.getMessageByChatID )
-route.get('/find/:chatID/:senderID',messageController.getMessageByBoth)
+route.get('/findbyboth',messageController.getMessageByBoth)
 module.exports = route
