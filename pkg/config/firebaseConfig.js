782bcf4baffc7f0c18e50deab90d45b6ec948836
DuongWuangDat <duongquangdat172004@gmail.com>
2024-04-03 08:22:50 +0700

    fix upload

---
+++ pkg/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+firebaseConfig = {
+        apiKey: "AIzaSyDBY3h-59d1LB8U_W54QveIEANAs3o3ZUU",
+        authDomain: "skillexchange-62da0.firebaseapp.com",
+        projectId: "skillexchange-62da0",
+        storageBucket: "skillexchange-62da0.appspot.com",
+        messagingSenderId: "620499387371",
+        appId: "1:620499387371:web:1d9a5bb2834406ee53cfae",
+        measurementId: "G-NP63D4FV2Y"
+    };
+module.exports = {firebaseConfig}
--- route/upload_route.js
+++ route/upload_route.js
@@ -1,40 +1,48 @@
+//import { initializeApp } from "firebase/app";
+//import { getStorage, ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+const {initializeApp} = require("firebase/app")
+const { getStorage, ref, getDownloadURL, uploadBytesResumable } = require("firebase/storage")
+
 const express = require("express")
 const route = express.Router()
+const config = require("../pkg/config/firebaseConfig")
 
 const multer = require("multer")
 
-const FILE_TYPE_NAP = {
-    'image/png': 'png',
-    'image/jpeg': 'jpeg',
-    'image/jpg': 'jpg'
-}
-
-const storage = multer.diskStorage({
-    destination: function (req,file, cb){
-        const isValid = FILE_TYPE_NAP[file.mimetype];
-        let uploadError = new Error('invalid image type');
-
-        if(isValid){
-            uploadError = null;
-        }
-        cb(uploadError, 'public/uploads')
-    },
-    filename: function (req, file, cb) {
-        const filename = file.originalname.split(' ').join('-');
-        const extension = FILE_TYPE_NAP[file.mimetype];
-        cb(null, `${filename}-${Date.now()}.${extension}`)
-    }
+initializeApp({
+    apiKey: "AIzaSyDBY3h-59d1LB8U_W54QveIEANAs3o3ZUU",
+    authDomain: "skillexchange-62da0.firebaseapp.com",
+    projectId: "skillexchange-62da0",
+    storageBucket: "skillexchange-62da0.appspot.com",
+    messagingSenderId: "620499387371",
+    appId: "1:620499387371:web:1d9a5bb2834406ee53cfae",
+    measurementId: "G-NP63D4FV2Y"
 })
 
-const uploadOptions = multer({storage: storage})
+const storage = getStorage();
 
-route.post("/upload", uploadOptions.single("image"), (req,res)=>{
+
+
+const uploadOptions = multer({storage: multer.memoryStorage()})
+
+route.post("/upload", uploadOptions.single("image"),async (req,res)=>{
+    
     const file = req.file;
     if(!file) return res.status(400).send('No image in the request')
-    const filename = file.filename;
-    const basePath = `${req.protocol}://${req.get('host')}/public/uploads`;
+    const curDate = new Date();
+    const curMili = curDate.getMilliseconds();
+
+    const storageRef = ref(storage, `files/${req.file.originalname}-${curMili}`)
+
+    const metaData= {
+        contentType: req.file.mimetype
+    }
+
+    const snapShot = await uploadBytesResumable(storageRef,req.file.buffer,metaData)
+
+    const downloadURL = await getDownloadURL(snapShot.ref)
     return res.json({
-        image: `${basePath}/${filename}`
+        image: downloadURL
     })
 })
 
