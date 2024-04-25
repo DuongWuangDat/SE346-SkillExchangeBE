41c2b724161b0ae8e83595ec88e841d613959877
DuongWuangDat <duongquangdat172004@gmail.com>
2024-04-25 17:57:09 +0700

    do smth

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ io.on("connection", (socket)=>{
     
     socket.on("sendMessage", (req)=>{
         console.log(req.recipicentID)
-        const user = onlineUsers.find((userFind) => userFind.userID === req.recipicentID)
+        const user = onlineUsers.find((userFind) => userFind.userID == req.recipicentID)
         console.log(user)
         if(user){
             io.to(user.socketID).emit("getMessage", req)
